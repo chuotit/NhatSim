1894dd5f86edcc99af0fdfa0557cb3e726b6de94
chuotit <tiencong.com@gmail.com>
2017-10-06 00:02:38 +0700

    Add models and abstracts

---
+++ NhatSim.Model/Abstract/Auditable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NhatSim.Model.Abstract
+{
+    public abstract class Auditable : IAuditable
+    {
+        [MaxLength(255)]
+        public string MetaDescription { set; get; }
+
+        [MaxLength(255)]
+        public string MetaKeyword { set; get; }
+
+        public DateTime? CreateDate { set; get; }
+
+        [MaxLength(255)]
+        public string CreateBy { set; get; }
+
+        public DateTime? UpdateDate { set; get; }
+
+        [MaxLength(255)]
+        public string UpdateBy { set; get; }
+
+        public bool Status { set; get; }
+
+        public int? DisplayOrder { set; get; }
+    }
+}
---
+++ NhatSim.Model/Abstract/IAuditable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NhatSim.Model.Abstract
+{
+    public interface IAuditable
+    {
+        string MetaDescription { set; get; }
+        string MetaKeyword { set; get; }
+
+        DateTime? CreateDate { set; get; }
+        string CreateBy { set; get; }
+
+        DateTime? UpdateDate { set; get; }
+        string UpdateBy { set; get; }
+
+        bool Status { set; get; }
+
+        int? DisplayOrder { set; get; }
+    }
+}
