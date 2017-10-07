e196c2a05ce9d4ae1f786976d1ee73f4424bad3d
chuotit <tiencong.com@gmail.com>
2017-10-07 15:06:44 +0700

    Add repositories base

---
+++ NhatSim.Data/Repositories/FirstNumberRepository.cs
@@ -0,0 +1,16 @@
+﻿using NhatSim.Data.Infrastructure;
+using NhatSim.Model.Models;
+
+namespace NhatSim.Data.Repositories
+{
+    public interface IFirstNumberRepository : IRepository<FirstNumber>
+    {
+    }
+
+    public class FirstNumberRepository : RepositoryBase<FirstNumber>, IFirstNumberRepository
+    {
+        public FirstNumberRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
